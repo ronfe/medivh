5b8ff9e709535e2f7ca040bef5a1af62d7b761c5
diggzhang <diggzhang@gmail.com>
2015-08-28 17:00:12 +0800

    modify shell script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/medivh
-sudo npm install
+cnpm install
+bower install
 gulp copy-seed1
-sudo npm start >> font-compress/template/seed1.jade
+cnpm start >> font-compress/template/seed1.jade
 gulp font-compress
 gulp release
